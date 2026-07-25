e114de6a7f2c15049cf4d8e49232b089fd270ab8
LakshyaK10 <lakshyakhamesra@gmail.com>
2026-07-26 01:20:42 +0530

    Time: 1 ms (99.89%), Space: 45.7 MB (11.5%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,33 +1,56 @@
 class Solution {
     public int compress(char[] chars) {
-        int n=chars.length;
-        if(n==1){
-            return 1;
-        }
-        String s="";
+        // int n=chars.length;
+        // if(n==1){
+        //     return 1;
+        // }
+        // String s="";
 
-        char prev=chars[0];
-        int count=1;
-        for(int i=1;i<n;i++){
-            if(prev==chars[i]){
+        // char prev=chars[0];
+        // int count=1;
+        // for(int i=1;i<n;i++){
+        //     if(prev==chars[i]){
+        //         count++;
+        //     }else{
+        //         s=s+prev;
+        //         if(count>1){
+        //             s=s+count;
+        //         }
+        //         prev=chars[i];
+        //         count=1;
+        //     }
+        // }
+        // s=s+prev;
+        // if(count>1){
+        //     s=s+count;
+        // }
+        // for(int i=0;i<s.length();i++){
+        //     chars[i]=s.charAt(i);
+        // }
+        // System.out.println(s);
+        // return s.length();
+
+
+        int read=0;
+        int write=0;
+
+        while(read<chars.length){
+            char curr=chars[read];
+            int count=0;
+
+            while(read<chars.length && curr==chars[read]){
+                read++;
                 count++;
-            }else{
-                s=s+prev;
-                if(count>1){
-                    s=s+count;
+            }
+
+            chars[write++]=curr;
+
+            if(count>1){
+                for(char c:String.valueOf(count).toCharArray()){
+                    chars[write++]=c;
                 }
-                prev=chars[i];
-                count=1;
             }
         }
-        s=s+prev;
-        if(count>1){
-            s=s+count;
-        }
-        for(int i=0;i<s.length();i++){
-            chars[i]=s.charAt(i);
-        }
-        System.out.println(s);
-        return s.length();
+        return write;
     }
 }
