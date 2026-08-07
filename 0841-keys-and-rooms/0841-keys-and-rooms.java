d284d0c8c8f9536bca4cc51c90c1cad650f01848
LakshyaK10 <lakshyakhamesra@gmail.com>
2026-08-07 17:18:50 +0530

    Time: 2 ms (45.13%), Space: 46.6 MB (11.69%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -5,15 +5,16 @@ class Solution {
         Queue<Integer> q=new LinkedList<>();
         boolean[] vis=new boolean[n];
         q.offer(0);
-        // vis[0]=true;
+        vis[0]=true;
 
 
         while(!q.isEmpty()){
             int cur=q.poll();
-            vis[cur]=true;
+            // vis[cur]=true;
 
             for(int room:rooms.get(cur)){
                 if(!vis[room]){
+                    vis[room]=true;
                     q.offer(room);
                 }
             }
