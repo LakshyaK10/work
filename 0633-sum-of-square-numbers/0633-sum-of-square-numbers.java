74c130a47f43d9ed50b81e44b37cda90f62f1796
LakshyaK10 <lakshyakhamesra@gmail.com>
2026-08-10 21:33:32 +0530

    Time: 5 ms (20%), Space: 42.1 MB (50.36%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int low=0;
+        int high=(int)Math.sqrt(c);
+
+        while(low<=high){
+            long sum=(long)low*low+(long)high*high;
+
+            if(sum==c){
+                return true;
+            }else if(sum<c){
+                low=low+1;
+            }else{
+                high--;
+            }
+        }
+        System.out.println(high);
+        return false;
+    }
+}
