ed80d3d88836f7827f15bd6f9bb6328ce9f6c731
LakshyaK10 <lakshyakhamesra@gmail.com>
2026-09-02 10:56:06 +0530

    Time: 6 ms (94.12%), Space: 63 MB (50.8%) - LeetHub

--- 3324-find-the-sequence-of-strings-appeared-on-the-screen/3324-find-the-sequence-of-strings-appeared-on-the-screen.java
+++ 3324-find-the-sequence-of-strings-appeared-on-the-screen/3324-find-the-sequence-of-strings-appeared-on-the-screen.java
@@ -1,18 +1,33 @@
 class Solution {
     public List<String> stringSequence(String target) {
-        String str="";
-        int n=target.length();
+        // String str="";
+        // int n=target.length();
+        // List<String> ans=new ArrayList<>();
+
+        // for(int i=0;i<n;i++){
+        //     str=str+'a';
+        //     ans.add(str);
+
+        //     while(str.charAt(str.length()-1)!=target.charAt(i)){
+        //         char l=str.charAt(str.length()-1);
+        //         l++;
+        //         str=str.substring(0,str.length()-1)+l;
+        //         ans.add(str);
+        //     }
+        // }
+        // return ans;
+
+
+        StringBuilder str=new StringBuilder();
         List<String> ans=new ArrayList<>();
 
-        for(int i=0;i<n;i++){
-            str=str+'a';
-            ans.add(str);
+        for(int i=0;i<target.length();i++){
+            str.append('a');
+            ans.add(str.toString());
 
             while(str.charAt(i)!=target.charAt(i)){
-                char l=str.charAt(i);
-                l++;
-                str=str.substring(0,i)+l;
-                ans.add(str);
+                str.setCharAt(i,(char)(str.charAt(i)+1));
+                ans.add(str.toString());
             }
         }
         return ans;
