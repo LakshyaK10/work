5ca076f429493526ad1f81034d3adce10ee7963e
LakshyaK10 <lakshyakhamesra@gmail.com>
2026-09-22 16:40:58 +0530

    Time: 279 ms (82.51%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,26 @@
 # Write your MySQL query statement below
-SELECT MAX(salary) AS SecondHighestSalary
-FROM Employee
-WHERE salary<(SELECT MAX(SALARY)
-            FROM Employee);
+-- SELECT MAX(salary) AS SecondHighestSalary
+-- FROM Employee
+-- WHERE salary<(SELECT MAX(SALARY)
+--             FROM Employee);
+
+
+-- SELECT (
+--     SELECT salary
+--     FROM Employee
+--     ORDER BY salary DESC
+--     LIMIT 1
+--     OFFSET 1
+-- ) AS SecondHighestSalary;
+
+
+SELECT (
+    SELECT DISTINCT salary
+    FROM (
+        SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+        FROM Employee
+    ) AS TempRank
+    WHERE rnk=2
+) AS SecondHighestSalary;
+
+
